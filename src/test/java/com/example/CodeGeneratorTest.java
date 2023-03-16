39ee3cef6571ae8ce8542998842a4073107e32b6
wei-yuan <wei.yuan@smartnews.com>
2023-03-16 23:04:45 +0900

    Add unit test for useJakarta feature

--- src/test/java/com/example/CodeGeneratorTest.java
+++ src/test/java/com/example/CodeGeneratorTest.java
@@ -37,4 +37,22 @@ public class CodeGeneratorTest {
         CodeGenerator.generateAll(config, false);
     }
 
+    @Test
+    public void _03_generateAll_TableScanMode_Is_Default_use_Jakarta_no_jsr305() throws Exception {
+        CodeGeneratorConfig config = CodeGeneratorConfig.load("entityGenConfig3.yml");
+        config.setJpa1SupportRequired(true);
+        config.setOutputDirectory("src/test/java");
+        CodeGenerator.generateAll(config, true);
+        CodeGenerator.generateAll(config, false);
+    }
+
+    @Test
+    public void _04_generateAll_TableScanMode_Is_RuleBaseduse_Jakarta_and_jsr305() throws Exception {
+        CodeGeneratorConfig config = CodeGeneratorConfig.load("entityGenConfig4.yml");
+        config.setJpa1SupportRequired(true);
+        config.setOutputDirectory("src/test/java");
+        CodeGenerator.generateAll(config, true);
+        CodeGenerator.generateAll(config, false);
+    }
+
 }
